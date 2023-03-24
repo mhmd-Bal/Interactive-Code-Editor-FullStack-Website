68712ec8f463dc78d9ca2aea540f21aa6e142411
RashaM <rashamassoud1998@gmail.com>
2023-03-25 01:19:42 +0200

    Modified the format of the api.php to be clean

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,28 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::group(["prefix" => "v0.0.0"], function(){
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::group(["middleware" => "auth:api"], function () {
-    Route::post('/users', [UserController::class, 'getUsers']);
-});
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
-    Route::get('me', 'me');
+    Route::group(["prefix" => "v0.0.0"], function(){
+        Route::post('/login',[AuthController:: class, "login"]);
+        Route::post('/register',[AuthController:: class, "register"]);
+        Route::post('/login',[AuthController:: class, "login"]);
+        Route::post('/refresh',[AuthController:: class, "refresh"]);
+        Route::get('/me',[AuthController:: class, "me"]);
+    });
 
+    Route::group(["middleware" => ["auth:api"]], function(){
+        //Routes demanding verifications 
+    });
 });
