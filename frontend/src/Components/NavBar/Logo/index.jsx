2f408ab3fd51e46dbdd21fe6ede2cdb248b39f24
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-25 22:37:51 +0200

    Added the lgo place in the navBar component

---
+++ frontend/src/Components/NavBar/Logo/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src={"Python.svg"} alt="" />
+    </div>
+  );
+};
+
+export default Logo;
