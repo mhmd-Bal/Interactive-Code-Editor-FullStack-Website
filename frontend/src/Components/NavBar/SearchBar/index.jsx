83332eb60ebc20c95536de07d85283713bf9cd0f
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-25 22:39:43 +0200

    Added the search bar to the navbar component

---
+++ frontend/src/Components/NavBar/SearchBar/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+const Search = () => {
+  return (
+    <div>
+      <input
+        className="searchBar"
+        type="search"
+        placeholder="Search for users"
+      />
+    </div>
+  );
+};
+
+export default Search;
