0fb5111b37eba34cf25bac566ae96a2667f7e67c
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-25 20:14:56 +0200

    Created the chat box page

---
+++ frontend/src/Pages/ChatBox/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatBox from "../../components/ChatBox";
+
+const ChatBoxPage = () => {
+  return (
+    <div>
+      <ChatBox />
+    </div>
+  );
+};
+
+export default ChatBoxPage;
