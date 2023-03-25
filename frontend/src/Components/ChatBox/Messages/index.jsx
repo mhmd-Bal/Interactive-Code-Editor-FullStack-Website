9b7b4483ab21e5c3a81b22e3667813097b4bee4a
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-25 20:07:51 +0200

    Added the messages component

---
+++ frontend/src/Components/ChatBox/Messages/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.css";
+
+const Message = () => {
+  return (
+    <div className="inbox">
+      <div className="message from">Hii</div>
+      <div className="message to">Hiii</div>
+      <div className="message to">Kifak</div>
+      <div className="message to">?</div>
+      <div className="message from">El hamdella</div>
+      <div className="message from">W enta?</div>
+    </div>
+  );
+};
+
+export default Message;
