e70795580f0642e7b6d9d85050a559da2aabf405
unknown <mohammad.balouza@gmail.com>
2023-03-25 23:23:07 +0200

    Added an unfinished homepage and added button component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { Routes, Route, Router } from "react-router-dom"
 import ChatBox from './Pages/ChatBox';
+import HomePage from './Pages/Home';
 
 function App() {
 
   return (
     <Routes>
-      {/* <Route path="/" element={<HomePage />} /> */}
+      <Route path="/" element={<HomePage />} />
       <Route path="/chat" element={<ChatBox />} />
       <Route path="*" element={<div>404</div>} />
     </Routes>
---
+++ frontend/src/Components/Button/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+function AppButton(props) {
+  return (
+    <button>{props.button_name}</button>
+  );
+}
+
+export default AppButton;
