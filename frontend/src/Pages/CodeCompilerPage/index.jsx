8b927e0ee02e622974b36975b9c926bc4f2cac35
unknown <mohammad.balouza@gmail.com>
2023-03-26 04:50:07 +0300

    Added the Compiler component

--- frontend/src/Pages/CodeCompilerPage/index.jsx
+++ frontend/src/Pages/CodeCompilerPage/index.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import CodeEditor from "../../Components/CodeEditor";
+import Compiler from "../../Components/Compiler";
 import NavBar from '../../Components/NavBar';
+import "./styles.css";
 
 const CodeCompilerPage = () => {
   return (
     <div style={{overflowX: "hidden"}}>
       <NavBar />
-      <CodeEditor />
+      <div className="code-section">
+        <CodeEditor />
+        <Compiler />
+      </div>
     </div>
   );
 };
