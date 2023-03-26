30d73929c8037ad37af625d9c0eb17f01b020fef
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-03-27 00:12:24 +0300

    Removed compileCode Function and compile Route from Backend

--- frontend/src/Components/Compiler/index.jsx
+++ frontend/src/Components/Compiler/index.jsx
@@ -1,32 +1,3 @@
-// import React, { useState } from "react";
-// import AppButton from "../Button";
-// import "./styles.css";
-
-// const Compiler = ({ output }) => {
-//   const [filename, setFilename] = useState("Shell");
-
-//   // const handleClearClick = () => {
-//   //   setOutput("");
-//   // };
-
-//   return (
-//     <div className="compiler-block">
-//       <div className="compiler-panel">
-//         <div className="compiler-filename">
-//           <p>{filename}</p>
-//         </div>
-//         <div className="compiler-controls">
-//           {/* <AppButton button_name="Clear" onClick={handleClearClick} /> */}
-//         </div>
-//       </div>
-//       <div className="compiler-output">
-//         <p>{output}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Compiler;
 import React from "react";
 import AppButton from "../Button";
 import "./styles.css";
@@ -36,7 +7,7 @@ const Compiler = ({ output, handleClearClick }) => {
     <div className="compiler-block">
       <div className="compiler-panel">
         <div className="compiler-filename">
-          <p>Shell</p>
+          <p>Console</p>
         </div>
         <div className="compiler-controls">
           <AppButton button_name="Clear" onClick={handleClearClick} />
--- frontend/src/Pages/CodeCompilerPage/index.jsx
+++ frontend/src/Pages/CodeCompilerPage/index.jsx
@@ -1,24 +1,3 @@
-// import React, { useState } from "react";
-// import CodeEditor from "../../Components/CodeEditor";
-// import Compiler from "../../Components/Compiler";
-// import NavBar from "../../Components/NavBar";
-// import "./styles.css";
-
-// const CodeCompilerPage = () => {
-//   const [output, setOutput] = useState("");
-
-//   return (
-//     <div style={{ overflowX: "hidden" }}>
-//       <NavBar />
-//       <div className="code-section">
-//         <CodeEditor setOutput={setOutput} />
-//         <Compiler output={output} setOutput={setOutput} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CodeCompilerPage;
 import React, { useState } from "react";
 import CodeEditor from "../../Components/CodeEditor";
 import Compiler from "../../Components/Compiler";
