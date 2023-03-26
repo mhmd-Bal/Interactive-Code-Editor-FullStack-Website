fdcd4056a5c10cf30eb40516028253b61eecaa04
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-03-26 21:08:03 +0300

    Added Post Request To Extrenal API to Compile Snippet Code

--- frontend/src/Components/CodeEditor/index.jsx
+++ frontend/src/Components/CodeEditor/index.jsx
@@ -17,7 +17,28 @@ const CodeEditor = () => {
   
   
   const handleCompileClick = () => {
-console.log("compiling");
+    const code = 'print("Hello, World!")';
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("LanguageChoice", "5");
+    encodedParams.append("Program", "print('Hello World')");
+    
+    const options = {
+      method: 'POST',
+      url: 'https://code-compiler.p.rapidapi.com/v2',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'X-RapidAPI-Key': 'c2f267bd0cmsh63f6cc991a18825p1066f7jsn631466f02f8b',
+        'X-RapidAPI-Host': 'code-compiler.p.rapidapi.com'
+      },
+      data: encodedParams
+    };
+    
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+
   };
 
   return (
