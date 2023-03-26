a9d59db25c3b9321b5320d76e734740165e15f01
unknown <mohammad.balouza@gmail.com>
2023-03-26 03:58:00 +0300

    Added the admin route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, Router } from "react-router-dom"
 import ChatBox from './Pages/ChatBoxPage';
 import HomePage from './Pages/HomePage';
 import LoginPage from './Pages/LoginPage';
+import AdminPannelPage from './Pages/AdminPannelPage';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/chat" element={<ChatBox />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/admin" element={<AdminPannelPage />} />
       <Route path="*" element={<div>404</div>} />
     </Routes>
   );
