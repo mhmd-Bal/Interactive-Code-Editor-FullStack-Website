b4281d15dae7693e00232cdc1fb7e03aaa9b8aa8
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-28 01:17:11 +0300

    Linked the chat box page back to the browse users page

--- frontend/src/Components/ChatBox/NameBar/index.jsx
+++ frontend/src/Components/ChatBox/NameBar/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./styles.css";
-const NameBar = () => {
+const NameBar = ({ back }) => {
   return (
     <div className="nameBar">
-      <img className="back" src={"Arrow.svg"} alt="" />
+      <button onClick={back}>Back</button>
       <div className="user">
         <img className="pic" src={"randomPic.webp"} alt="" />
         <div className="userName">User Name</div>
