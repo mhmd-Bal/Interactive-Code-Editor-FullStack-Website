6e234c28c3c2687c5ab7f57e95d1ea8be85dec78
unknown <mohammad.balouza@gmail.com>
2023-03-27 20:24:19 +0300

    Added navigation for the nav buttons

--- frontend/src/Components/NavBar/Buttons/index.jsx
+++ frontend/src/Components/NavBar/Buttons/index.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import AppButton from "../../Button";
 import "./styles.css";
+import { useNavigate } from "react-router-dom";
 
 function NavButtons() {
+
+  const navigate = useNavigate();
+
+  const handleNavigateToLogin = () => {
+    navigate("/login");
+  }
+
+  const handleNavigateToRegister = () => {
+    navigate("/register");
+  }
+
   return (
     <div className="navButtons">
       <div className="buttonHolder">
         {/* <button className="buttons">Home</button>
         <button className="buttons">Log Out</button>
         <button className="buttons">Start Coding</button> */}
-        <AppButton button_name="Login" />
-        <AppButton button_name="Signup" />
+        <AppButton button_name="Login" onClick={handleNavigateToLogin} />
+        <AppButton button_name="Signup" onClick={handleNavigateToRegister} />
       </div>
 
       <img className="profile" src={"Profile.svg"} alt="" />
