0854ab3e102f3850b5af813e2beb8de2b18c9614
unknown <mohammad.balouza@gmail.com>
2023-03-27 20:44:10 +0300

    Added the logout feature

--- frontend/src/Components/NavBar/Buttons/index.jsx
+++ frontend/src/Components/NavBar/Buttons/index.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import AppButton from "../../Button";
 import "./styles.css";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function NavButtons() {
 
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
-  console.log(token);
 
   const handleNavigateToLogin = () => {
     navigate("/login");
@@ -27,7 +27,21 @@ function NavButtons() {
 
   const handleLogout = () => {
     localStorage.clear();
-    navigate("/");
+    
+    const logout = {
+      method: 'POST',
+      url: 'http://127.0.0.1:8000/api/v0.0.1/logout/',
+      headers: {
+        'content-type': 'application/json',
+        'Accept' : 'application/json',
+        'Authorization': 'bearer ' + token
+      },
+    };
+    
+    axios.request(logout)
+        .then(response => {
+            navigate("/");
+      });
   }
 
   return (
@@ -37,7 +51,6 @@ function NavButtons() {
         <AppButton button_name="Signup" onClick={handleNavigateToRegister} />
       </div>
       <div className={token ? "buttonHolder" : "disabled"}>
-
         <AppButton button_name="Browse Users"  onClick={handleNavigateToBrowseUsers} />
         <AppButton button_name="Start Coding" onClick={handleNavigateToCompileCode} />
         <AppButton button_name="Logout" onClick={handleLogout} />
