feca6c4e01560b80ec69ee6be7a601cdcf3e76f9
RashaM <rashamassoud1998@gmail.com>
2023-03-28 02:23:26 +0300

    Added the logout buttun to the admin's page

--- frontend/src/Components/Admin/Header/index.jsx
+++ frontend/src/Components/Admin/Header/index.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
 import { FaBars } from 'react-icons/fa';
+import AppButton from "../../Button";
 import "./style.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Header = ({ toggle }) => {
-    return (
-        <header >
-          <nav className='navbar'>
-            <div className='nav-icon' onClick={toggle}>
-              <FaBars />
-            </div>
-            <div ><h1>Admin Pannel</h1></div>
-          </nav>
-        </header>
-    );
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+
+    const token = localStorage.getItem("token");
+    
+    localStorage.clear();
+    
+    const logout = {
+      method: 'POST',
+      url: 'http://127.0.0.1:8000/api/v0.0.1/logout/',
+      headers: {
+        'content-type': 'application/json',
+        'Accept' : 'application/json',
+        'Authorization': 'bearer ' + token
+      },
+    };
+    
+    axios.request(logout)
+        .then(response => {
+            navigate("/");
+      });
+  }
+  return (
+    <header >
+      <nav className='navbar'>
+        <div className='nav-icon' onClick={toggle}>
+          <FaBars />
+        </div>
+        <div ><h1>Admin Pannel</h1></div>
+        <AppButton button_name="Logout" onClick={handleLogout} />
+
+      </nav>
+    </header>
+  );
 };
 export default Header;
