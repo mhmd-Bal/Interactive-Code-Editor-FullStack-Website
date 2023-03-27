cb7adfcc1c8b16ef41fd11a360d62bced932c704
unknown <mirvatbarakat1@gmail.com>
2023-03-27 03:18:18 +0300

    Added linking to getUseres API

--- frontend/src/Pages/AdminPannelPage/index.jsx
+++ frontend/src/Pages/AdminPannelPage/index.jsx
@@ -1,9 +1,30 @@
-import React , {useState} from "react";
+import React , {useState, useEffect} from "react";
 import Header from  '../../Components/Admin/Header';
 import Sidebar from '../../Components/Admin/Sidebar';
+import axios from 'axios';
 
 const AdminPage = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(true);
+    const [users, setUsers] = useState([]);
+    const token = sessionStorage.getItem("token");
+
+    const getUsers = {
+      method: 'GET',
+      url: 'http://127.0.0.1:8000/api/v0.0.1/users/',
+      headers: {
+        'content-type': 'application/json',
+        'Accept' : 'application/json',
+        'Authorization': 'bearer ' + token
+      },
+    };
+
+    useEffect(() => {
+      axios.request(getUsers)
+          .then(response => {
+              setUsers(response.data.users);
+              console.log(response);
+          })
+    },[]);
 
     const toggle = () => {
       setIsOpen(!isOpen);
