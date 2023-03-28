22d8e3d5ef553c7896a94b0a8b55d9daf7abb1d9
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2023-03-28 10:06:12 +0300

    Revert "Added autocompleteoff to input elements in register page"
    This reverts commit fc67e869acd835ddda787ded741078c8486d1ea9.

--- frontend/src/Components/Register/index.jsx
+++ frontend/src/Components/Register/index.jsx
@@ -65,15 +65,15 @@ const RegisterBlock = () => {
             <form onSubmit={handleFormSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">Username:</label>
-                    <input type="text" id="username" name="username" value={username} onChange={handleUsernameChange} required autoComplete="off"/>
+                    <input type="text" id="username" name="username" value={username} onChange={handleUsernameChange} required/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" value={email} onChange={handleEmailChange} required autoComplete="off"/>
+                    <input type="email" id="email" name="email" value={email} onChange={handleEmailChange} required/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password" value={password} onChange={handlePasswordChange} required autoComplete="off"/>
+                    <input type="password" id="password" name="password" value={password} onChange={handlePasswordChange} required/>
                     {passwordError && <div className="error">{passwordError}</div>}
                 </div>
                 <button type="submit">Register</button>
