ac3f88d2b38bbc491ee780b4e44d4fba1b97476d
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-28 08:48:29 +0300

    Retrieved the message receiver name in the ChatBox

--- frontend/src/Components/ChatBox/NameBar/index.jsx
+++ frontend/src/Components/ChatBox/NameBar/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./styles.css";
-const NameBar = ({ back }) => {
+const NameBar = ({ back, reseiver_name }) => {
   return (
     <div className="nameBar">
       <button onClick={back}>Back</button>
       <div className="user">
         <img className="pic" src={"randomPic.webp"} alt="" />
-        <div className="userName">User Name</div>
+        <div className="userName">{reseiver_name}</div>
       </div>
     </div>
   );
